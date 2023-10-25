5d5e385ebc723f4b5254fff335831ae8a7ed1071
enrperes@ubuntu <enrico.peressin@gmail.com>
2023-10-25 16:10:39 +0200

    Added reverse string algorithms in cpp and py

--- 4-prefissi.py
+++ 4-prefissi.py
@@ -1,3 +1,4 @@
 string = input("insert a string: ")
-for i in range(len(string) -1, -1, -1):
+
+for i in range(len(string) -1, -1, -1): # len(string)-1 = starting index, -1 = ending index, -1 = step
     print(string[:i])
---
+++ 5-reverse.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string str;
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, str);
+    for (int i = str.length() - 1; i >= 0; i--) {
+        std::cout << str[i];
+    }
+    std::cout << std::endl; 
+    return 0;
+}   
