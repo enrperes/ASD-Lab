b2efdc230b2d2324fa6e231e0544093fd715bdf0
enrperes@linuxOS <enrico.peressin@gmail.com>
2023-11-22 10:32:09 +0100

    6-binary-search.py file

--- 6-binary-search.py
+++ 6-binary-search.py
@@ -1,42 +1,28 @@
+raw_input = input
 
-# def binary_search(arr, key):
-#     """
-#     Perform binary search on a sorted array of integers.
+def getArray():
+	tokens = raw_input().split(" ")
+	return [int(x) for x in tokens if x]  # "if x" filters out empty tokens
 
-#     Args:
-#     - arr: a sorted array of integers
-#     - key: the integer to search for in the array
+def binarySearchRec(array, left, right, item):
+	i = int(item)
+	if right >= left:
+		middle = left + (right - left) //2
+		if (i == array[middle]):
+			return middle
+		elif (array[middle] > i):
+			return binarySearch(array, left , middle - 1,  itemToFind)
+		else:
+			return binarySearch(array, middle + 1, right,  itemToFind)
+	else:
+		return -1
+	
+def binarySearch(array, item):
+	return binarySearchRec(array, 0, len(array)-1, item)
 
-#     Returns:
-#     - the index of the key in the array, if it exists (0-indexed)
-#     - -1 if the key is not present in the array
-#     """
-#     left = 0
-#     right = len(arr) - 1
+V = getArray()
+itemToFind = raw_input()
 
-#     while left <= right:
-#         mid = (left + right) // 2
-#         if arr[mid] == key:
-#             return mid
-#         elif arr[mid] < key:
-#             left = mid + 1
-#         else:
-#             right = mid - 1
+pos = binarySearch(V, itemToFind) + 1
 
-#     return -1
-
-
-def binary_search(a, low, high, key):
-    if high - low <= 0:
-        return -1
-    m = (low + high) // 2 
-    if a[m] == key:
-        return m
-    elif a[m] < key:
-        return binary_search(a, m + 1, high, key)
-    else:
-        return binary_search(a, low, m, key)
-    
-a = [int(x) for x in input().split(" ") if x]
-
-binary_search(a, 0, len(a), int(input()))   
+print(pos)
