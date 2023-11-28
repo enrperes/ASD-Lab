278e126610ba363f5a21209b29e09a6421e6686f
enrperes@linuxOS <enrico.peressin@gmail.com>
2023-11-28 18:57:11 +0100

    bohbahboh

--- 9-array-interval-sum.py
+++ 9-array-interval-sum.py
@@ -1,3 +1,4 @@
+
 # input: array di int V > 0, non ordinato. 
 # int S > 0
 # output: intervallo [i, j] di posizioni in V con i <= j, tale che: 
@@ -10,23 +11,36 @@
 
 # Implementare versione di O(n^3) e Theta(n) (c'è un errore, da correggere.)
 
-def findInterval(V, S):
-    i = j = 0
-    current_sum = V[i]
-    while i <= j < len(V):
-        # current_sum = sum a[i:j+1]
-        if current_sum == S:
-            return (i, j)
-        elif current_sum < S:
+def findInterval_slow(V, S):
+    for i in range(len(V)):
+        for j in range(i, len(V)):
+            if sum(V[i:j+1]) == S:
+                return (i, j)
+    return (-1, -1)
+
+def findInterval_fast(V, S):
+    i = 0
+    j = 0
+    sum = 0
+    while i < len(V):
+        if sum == S:
+            return (i+1, j-1)
+        elif sum < S:
+            sum += V[j]
             j += 1
-            current_sum += V[j]
         else:
-            current_sum -= V[i]
+            sum -= V[i]
             i += 1
-    return -1, -1
+    return (-1, -1)
 
 # get input
 V = [int(x) for x in input().split(" ") if x]
 S = int(input())
-i, j = findInterval(V, S)
+i, j = findInterval_slow(V, S)
 print(i, j)
+
+
+
+
+
+
